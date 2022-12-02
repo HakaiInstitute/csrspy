143638dba5558d955c435561956965f456aa457f
Taylor Denouden <taylordenouden@gmail.com>
2022-12-02 09:30:55 -0800

    Allow WGS84 + GRS80 input

--- csrspy/main.py
+++ csrspy/main.py
@@ -196,7 +196,7 @@ class CSRSTransformer(object):
         if self.is_itrf(ref_frame):
             assert vd is VerticalDatum.GRS80, f"{ref_frame} must use VerticalDatum GRS80."
         elif self.is_wgs84(ref_frame):
-            assert vd is VerticalDatum.WGS84, f"{ref_frame} must use VerticalDatum WGS84."
+            assert (vd is VerticalDatum.WGS84) or (vd is VerticalDatum.GRS80), f"{ref_frame} must use VerticalDatum WGS84 or GRS80."
         elif self.is_nad83(ref_frame):
             assert vd is not VerticalDatum.WGS84, f"{ref_frame} must not use VerticalDatum WGS84."
 
